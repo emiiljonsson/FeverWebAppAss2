0030ee25fa8a0111fd8f31f7df2fb52010c0777a
Emil Jonsson <e.jonsson1996@gmail.com>
2024-06-10 09:30:44 +0200

    first commit

---
+++ FeverWebApp/Controllers/DoctorController.cs
@@ -0,0 +1,25 @@
+﻿using FeverWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeverWebApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult FeverCheck()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult CheckFever(float temperature, string scale)
+        {
+            string message = TemperatureChecker.CheckTemperature(temperature, scale);
+            ViewBag.Message = message;
+            return View("FeverCheck"); 
+        }
+    }
+}
---
+++ FeverWebApp/Models/TemperatureChecker.cs
@@ -0,0 +1,26 @@
+﻿namespace FeverWebApp.Models
+{
+    public static class TemperatureChecker
+    {
+        public static string CheckTemperature(float temperature, string scale)
+        {
+            if (scale == "Fahrenheit")
+            {
+                temperature = (temperature - 32) * 5 / 9; // Convert to Celsius
+            }
+
+            if (temperature >= 38.0)
+            {
+                return "You have a fever.";
+            }
+            else if (temperature <= 35.0)
+            {
+                return "You might have hypothermia.";
+            }
+            else
+            {
+                return "Your temperature is normal.";
+            }
+        }
+    }
+}
